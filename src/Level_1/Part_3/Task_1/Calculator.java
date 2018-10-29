b695c5594fb1b02489aeb432fb9e1914841d6e2d
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-10-30 01:51:22 +0300

    Исправлено название метода на calculator

--- src/Level_1/Part_3/Task_1/Calculator.java
+++ src/Level_1/Part_3/Task_1/Calculator.java
@@ -2,67 +2,78 @@ package Level_1.Part_3.Task_1;
 
 public class Calculator {
     //summ
-    double sum (double a,double b ){
+    double sum(double a, double b) {
         double calculate;
-        calculate = a+b;
+        calculate = a + b;
         return calculate;
     }
-    int sum (int a,int b ){
+
+    int sum(int a, int b) {
         int calculate;
-        calculate = a+b;
+        calculate = a + b;
         return calculate;
     }
-    long sum (long a,long b ){
+
+    long sum(long a, long b) {
         long calculate;
-        calculate = a+b;
+        calculate = a + b;
         return calculate;
     }
+
     // subtraction
-    double subtraction (double a,double b ){
+    double subtraction(double a, double b) {
         double calculate;
-        calculate = a-b;
+        calculate = a - b;
         return calculate;
     }
-    int subtraction (int a,int b ){
+
+    int subtraction(int a, int b) {
         int calculate;
-        calculate = a-b;
+        calculate = a - b;
         return calculate;
     }
-    long subtraction (long a,long b ){
+
+    long subtraction(long a, long b) {
         long calculate;
-        calculate = a-b;
+        calculate = a - b;
         return calculate;
     }
+
     //multiplication
-    double multiplication (double a,double b ){
+    double multiplication(double a, double b) {
         double calculate;
-        calculate = a*b;
+        calculate = a * b;
         return calculate;
     }
-    int multiplication (int a,int b ){
+
+    int multiplication(int a, int b) {
         int calculate;
-        calculate = a*b;
+        calculate = a * b;
         return calculate;
     }
-    long multiplication (long a,long b ){
+
+    long multiplication(long a, long b) {
         long calculate;
-        calculate = a*b;
+        calculate = a * b;
         return calculate;
     }
+
     //division
-    double division (double a,double b ){
+    double division(double a, double b) {
         double calculate;
-        calculate = a/b;
+        calculate = a / b;
         return calculate;
     }
-    int division (int a,int b ){
+
+    int division(int a, int b) {
         int calculate;
-        calculate = a/b;
+        calculate = a / b;
         return calculate;
     }
-    long division (long a,long b ){
+
+    long division(long a, long b) {
         long calculate;
-        calculate = a/b;
+        calculate = a / b;
         return calculate;
     }
 }
--- src/Level_1/Part_3/Task_1/Main.java
+++ src/Level_1/Part_3/Task_1/Main.java
@@ -12,12 +12,12 @@ package Level_1.Part_3.Task_1;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculatorVar2 = new Calculator();
+        Calculator calculator = new Calculator();
 
-        System.out.println("Result sum: " + calculatorVar2.sum(5, 4));
-        System.out.println("Result subtraction: " + calculatorVar2.subtraction(5.02, 4));
-        System.out.println("Result multiplication: " + calculatorVar2.multiplication(5000, 4.2));
-        System.out.println("Result division: " + calculatorVar2.division(5, 4.02));
+        System.out.println("Result sum: " + calculator.sum(5, 4));
+        System.out.println("Result subtraction: " + calculator.subtraction(5.02, 4));
+        System.out.println("Result multiplication: " + calculator.multiplication(5000, 4.2));
+        System.out.println("Result division: " + calculator.division(5, 4.02));
 
 
     }
