b9acf90524be4978a5bbd93df4934bb8a9e5ef8f
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-10-30 01:32:51 +0300

    решение задачи № 1

---
+++ src/Level_1/Part_3/Task_1/Main.java
@@ -0,0 +1,15 @@
+package Level_1.Part_3.Task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculatorVar2 = new Calculator();
+
+        System.out.println("Result sum: " + calculatorVar2.sum(5, 4));
+        System.out.println("Result subtraction: " + calculatorVar2.subtraction(5.02, 4));
+        System.out.println("Result multiplication: " + calculatorVar2.multiplication(5000, 4.2));
+        System.out.println("Result division: " + calculatorVar2.division(5, 4.02));
+
+
+    }
+}
+
