3f7c81abcf144c00be2c1896dd992b55c7e5bd1a
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-10-31 11:17:03 +0300

    Решение 2 задачи (выборка по автору). Похоже метод searchByAuthor не нужен вообще. Сейчас с ним все работает, кроме couta

---
+++ src/Level_1/Part_3/Task_2/Task_2Qvest/Main.java
@@ -0,0 +1,113 @@
+package Level_1.Part_3.Task_2.Task_2Qvest;
+
+
+
+/*
+Задать критерий выбора данных и вывести эти данные на консоль.
+Book: id, Автор, Название, Год издания, Страна, Спецификация, Магазин.
+Создать массив объектов. Вывести:
+1 Список книг одного автора;*/
+
+
+public class Main {
+
+    int count;
+
+    public static void main(String[] args) {
+        Book[] books = new Book[9];
+
+        books[0] = new Book(1, "Ершов", "Конек Горбунок", 1895, "Россия", "Сказка", 10);
+        books[1] = new Book(2, "Салтыков-Щедрин", "Городовой", 1898, "Россия", "Проза", 9);
+        books[2] = new Book(3, "Филатов", "Сказка про Федота стрельца", 1987, "Россия", "Сказка", 10);
+        books[3] = new Book(4, "Салтыков-Щедрин", "Городовой-2", 1895, "Россия", "Сказка", 3);
+        books[4] = new Book(5, "Пушкин", "Сборник", 1865, "Россия", "Сказка", 1);
+        books[5] = new Book(6, "Пушкин", "Сборник-1", 1861, "Россия", "Сказка", 10);
+        books[6] = new Book(7, "Пушкин", "Сборник-2", 1862, "Россия", "Сказка", 2);
+        books[7] = new Book(8, "Пушкин", "Сборник-3", 1863, "Россия", "Сказка", 3);
+        books[8] = new Book(9, "Дюма", "Трое из леса", 1895, "Франция", "Роман", 9);
+
+        printSearchByAuthor(books, "Пушкин");
+
+
+    }
+
+    private static void printSearchByAuthor(Book[] books, String author) {
+        int count = 0;
+        System.out.println("\n Перечень книг автора: " + author);
+        for (int i = 0; i < books.length; i++) {
+            /*if (author != null && author.equals((books[i].getAuthor()))) {
+                System.out.println("\n" + author);
+                books[i].showDetails();*/
+            books[i].searchByAuthor(author);
+            count++;
+        }
+
+        System.out.println("Всего найдено: " + count + " книг(-а)");//Не работает
+    }
+}
+
+class Book {
+
+    int id;
+    String author;
+    String bookTitle;
+    int publicationYear;
+    String country;
+    String specification;
+    int stores;
+    int count;
+
+    Book(int id, String author, String bookTitle, int publicationYear, String country, String specification, int pointOfSale) {
+        this.id = id;
+        this.author = author;
+        this.bookTitle = bookTitle;
+        this.publicationYear = publicationYear;
+        this.country = country;
+        this.specification = specification;
+        this.stores = pointOfSale;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getAuthor() {return author;}
+
+    String getBookTitle() {
+        return bookTitle;
+    }
+
+    int getPublicationYear() {
+        return publicationYear;
+    }
+
+    String getCountry() {
+        return country;
+    }
+
+    String getSpecification() {
+        return specification;
+    }
+
+    int getStores() {
+        return stores;
+    }
+
+
+    void showDetails() {
+        System.out.println("\nid: " + getId());
+        System.out.println("author: " + getAuthor());
+        System.out.println("bookTitle: " + getBookTitle());
+        System.out.println("publicationDate: " + getPublicationYear());
+        System.out.println("country: " + getCountry());
+        System.out.println("specification: " + getSpecification());
+        System.out.println("stores: " + getStores());
+    }
+    public void searchByAuthor(String author) {
+
+        if (author != null && author.equals((getAuthor()))) {
+            System.out.println("\n" + author);
+            showDetails();
+        }
+    }
+}
