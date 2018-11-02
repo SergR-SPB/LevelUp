0c9982210f88d6dc6f3f874e0fcb647b43ae3033
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-11-02 15:56:27 +0300

    В работе

---
+++ src/Level_1/Part_3/Bookstore/Main_StoreRunner.java
@@ -0,0 +1,53 @@
+package Level_1.Part_3.Bookstore;
+
+public class Main_StoreRunner {
+    public static void main(String[] args) {
+        StoreBooks store = new StoreBooks() {
+            @Override
+            public void addProductToCatalog(AbstractPrintedEdition product) {
+                super.addProductToCatalog(product);
+            }
+
+            @Override
+            public AbstractPrintedEdition saleProductInCatalog() {
+                return super.saleProductInCatalog();
+            }
+
+            @Override
+            public void printProducts() {
+                super.printProducts();
+            }
+
+            @Override
+            public AbstractPrintedEdition mostCost() {
+                return super.mostCost();
+            }
+
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return super.equals(obj);
+            }
+
+            @Override
+            protected Object clone() throws CloneNotSupportedException {
+                return super.clone();
+            }
+
+            @Override
+            public String toString() {
+                return super.toString();
+            }
+
+            @Override
+            protected void finalize() throws Throwable {
+                super.finalize();
+            }
+        }
+
+    }
+}
