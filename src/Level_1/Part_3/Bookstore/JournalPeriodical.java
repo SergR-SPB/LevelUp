a3f842bc92d328b61e7bcec04b42478cab983a73
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-11-02 16:46:29 +0300

    Переподчинил классу Воок

--- src/Level_1/Part_3/Bookstore/JournalPeriodical.java
+++ src/Level_1/Part_3/Bookstore/JournalPeriodical.java
@@ -1,7 +1,7 @@
 package Level_1.Part_3.Bookstore;
 
-public class JournalPeriodical extends AbstractPrintedEdition {
-    public JournalPeriodical(){
-        super();
+public class JournalPeriodical extends Periodical {
+    public JournalPeriodical(String id) {
+        super(id);
     }
 }
