c7f1c082d6f538105aef779a1540994c78e0b70d
SergR-SPB <37777794+SergR-SPB@users.noreply.github.com>
2018-11-02 16:48:19 +0300

    hg

--- src/Level_1/Part_3/Bookstore/JournalPeriodical.java
+++ src/Level_1/Part_3/Bookstore/JournalPeriodical.java
@@ -3,5 +3,7 @@ package Level_1.Part_3.Bookstore;
 public class JournalPeriodical extends Periodical {
     public JournalPeriodical(String id) {
         super(id);
+        System.out.println(id);
     }
+
 }
